b7f94436b32461fc4ef1feb3d4e4aea79153eaa9
swamydce1 <57559594+swamydce1@users.noreply.github.com>
2021-01-10 22:21:50 +0000

    Initial Commit

---
+++ ClippingAUTO.py
@@ -0,0 +1,262 @@
+# USAGE
+# python ncs_realtime_objectdetection.py --graph graphs/mobilenetgraph --display 1
+# python ncs_realtime_objectdetection.py --graph graphs/mobilenetgraph --confidence 0.5 --display 1
+from time import sleep
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+GPIO.setup(18,GPIO.OUT)
+#GPIO.setup(22,GPIO.OUT)
+
+# import the necessary packages
+from mvnc import mvncapi as mvnc
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import numpy as np
+import time
+import cv2
+
+CLASSES = ["background", "dog"]
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# frame dimensions should be sqaure
+PREPROCESS_DIMS = (300, 300)
+DISPLAY_DIMS = (900, 900)
+
+# calculate the multiplier needed to scale the bounding boxes
+DISP_MULTIPLIER = DISPLAY_DIMS[0] // PREPROCESS_DIMS[0]
+def Cembre_forward(Mot1, Mot2):    
+    GPIO.output(Mot1,GPIO.HIGH)
+    GPIO.output(Mot2,GPIO.LOW)
+
+def Cembre_backward(Mot1, Mot2):    
+    GPIO.output(Mot1,GPIO.LOW)
+    GPIO.output(Mot2,GPIO.HIGH)
+
+def preprocess_image(input_image):
+    # preprocess the image
+    #print("process image")
+    preprocessed = cv2.resize(input_image, PREPROCESS_DIMS)
+    preprocessed = preprocessed - 127.5
+    preprocessed = preprocessed * 0.007843
+    preprocessed = preprocessed.astype(np.float16)
+
+    # return the image to the calling function
+    return preprocessed
+
+def predict(image, graph):
+    GPIO.output(18,GPIO.LOW)
+    #GPIO.output(22,GPIO.LOW)
+    # preprocess the image
+    #image = preprocess_image(image)
+
+    # send the image to the NCS and run a forward pass to grab the
+    # network predictions
+    #print("going to process image")
+    image = preprocess_image(image)
+    #print("start queue inference")
+    #graph.queue_inference_with_fifo_elem (input_fifo, output_fifo, image, image)
+    #print("success queue inference")
+    graph.LoadTensor(image, None)
+    #print("load graph")
+    (output, _) = graph.GetResult()
+    #print("outpout")
+    #(output, _) = output_fifo.read_elem()
+    #print("success read_element")
+    # grab the number of valid object predictions from the output,
+    # then initialize the list of predictions
+    num_valid_boxes = output[0]
+     
+    #print ('output', num_valid_boxes)
+    predictions = []
+    #print("num_valid_boxes",num_valid_boxes)
+    # loop over results
+
+    # loop over results
+    for box_index in range(int(num_valid_boxes)):
+        # calculate the base index into our array so we can extract
+        # bounding box information
+        base_index = 7 + box_index * 7
+        pre_class = int(output[base_index + 1])
+        #confi = output[base_index +2]
+        #if confi >= 95:
+        if output[base_index +2] >= 0.995: 
+            #print ("pred class", output[base_index + 1])
+            print ("Confidence 1111", output[base_index +2])
+            GPIO.output(18,GPIO.HIGH)
+            #GPIO.output(22,GPIO.LOW)
+            sleep(3)
+            GPIO.output(18,GPIO.LOW)
+            sleep(3)
+            #GPIO.output(22,GPIO.LOW)
+
+       
+
+        # boxes with non-finite (inf, nan, etc) numbers must be ignored
+        if (not np.isfinite(output[base_index]) or
+            not np.isfinite(output[base_index + 1]) or
+            not np.isfinite(output[base_index + 2]) or
+            not np.isfinite(output[base_index + 3]) or
+            not np.isfinite(output[base_index + 4]) or
+            not np.isfinite(output[base_index + 5]) or
+            not np.isfinite(output[base_index + 6])):
+            continue
+
+        # extract the image width and height and clip the boxes to the
+        # image size in case network returns boxes outside of the image
+        # boundaries
+        (h, w) = image.shape[:2]
+        
+        x1 = max(0, int(output[base_index + 3] * w))
+        y1 = max(0, int(output[base_index + 4] * h))
+        x2 = min(w, int(output[base_index + 5] * w))
+        y2 = min(h, int(output[base_index + 6] * h))
+
+        # grab the prediction class label, confidence (i.e., probability),
+        # and bounding box (x, y)-coordinates
+        #print("pred class",output[base_index + 1])
+        pred_class = int(output[base_index + 1])
+        pred_conf = output[base_index + 2]
+        pred_boxpts = ((x1, y1), (x2, y2))
+
+        # create prediciton tuple and append the prediction to the
+        # predictions list
+        prediction = (pred_class, pred_conf, pred_boxpts)
+        #print (pred_class, pred_conf)
+        predictions.append(prediction)
+
+    # return the list of predictions to the calling function
+    return predictions
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-g", "--graph", default="mobilenetgraph",
+#ap.add_argument("-g", "--graph", default="graphs/mobilenetgraph",
+help="path to input graph file")
+ap.add_argument("-c", "--confidence", default=.5,
+help="confidence threshold")
+ap.add_argument("-d", "--display", type=int, default=1,
+help="switch to display image on screen")
+args = vars(ap.parse_args())
+
+# grab a list of all NCS devices plugged in to USB
+#print("[INFO] finding NCS devices...")
+devices = mvnc.EnumerateDevices()
+
+# if no devices found, exit the script
+if len(devices) == 0:
+    print("[INFO] No devices found. Please plug in a NCS")
+    quit()
+
+# use the first device since this is a simple test script
+# (you'll want to modify this is using multiple NCS devices)
+#print("[INFO] found {} devices. device0 will be used. "
+#    "opening device0...".format(len(devices)))
+device = mvnc.Device(devices[0])
+device.OpenDevice()
+
+# open the CNN graph file
+#print("[INFO] loading the graph file into RPi memory...")
+with open(args["graph"], mode="rb") as f:
+    graph_in_memory = f.read()
+
+# load the graph into the NCS
+#print("[INFO] allocating the graph on the NCS...")
+graph = device.AllocateGraph(graph_in_memory)
+# open a pointer to the video stream thread and allow the buffer to
+# start to fill, then start the FPS counter
+#print("[INFO] starting the video stream and FPS counter...")
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(1)
+fps = FPS().start()
+#print("[INFO] creating fifo ")
+#input_fifo, output_fifo = graph.allocate_with_fifos(device, graph_in_memory)
+#print("[INFO] fifo created")
+# loop over frames from the video file stream
+while True:
+    try:
+        # grab the frame from the threaded video stream
+        # make a copy of the frame and resize it for display/video purposes
+        frame = vs.read() 
+        
+        #print("[INFO] start predict", frame.shape)
+        image_for_result = frame.copy()
+        image_for_result = cv2.resize(image_for_result, DISPLAY_DIMS)
+
+        # use the NCS to acquire predictions 
+        #print("[INFO] start predict")
+
+        predictions = predict(frame, graph)
+        #print("[INFO] success predict")
+        # loop over our predictions
+        for (i, pred) in enumerate(predictions):
+            # extract prediction data for readability
+            (pred_class, pred_conf, pred_boxpts) = pred
+            #print (pred_class, pred_conf, pred_boxpts)
+
+
+            if args["display"] > 0:
+                # build a label consisting of the predicted class and
+                # associated probability
+                label = "{}: {:.2f}%".format(CLASSES[pred_class],
+                    pred_conf * 100)
+                fps.stop()
+                cv2.putText(image_for_result, "FPS = {:.2f}".format(fps.fps()), (100,100), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 2)
+                # extract information from the prediction boxpoints
+                (ptA, ptB) = (pred_boxpts[0], pred_boxpts[1])
+                ptA = (ptA[0] * DISP_MULTIPLIER, ptA[1] * DISP_MULTIPLIER)
+                ptB = (ptB[0] * DISP_MULTIPLIER, ptB[1] * DISP_MULTIPLIER)
+                (startX, startY) = (ptA[0], ptA[1])
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+
+                # display the rectangle and label text
+                cv2.rectangle(image_for_result, ptA, ptB,
+                    COLORS[pred_class], 2)
+                cv2.putText(image_for_result, label, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, COLORS[pred_class], 3)
+
+        # check if we should display the frame on the screen
+        # with prediction data (you can achieve faster FPS if you
+        # do not output to the screen)
+        #if args["display"] > 0:
+        #    # display the frame to the screen
+        #    cv2.imshow("Output", image_for_result)
+        #    key = cv2.waitKey(1) & 0xFF
+
+            # if the `q` key was pressed, break from the loop
+        #    if key == ord("q"):
+        #        break
+
+        # update the FPS counter
+        fps.update()
+    
+    # if "ctrl+c" is pressed in the terminal, break from the loop
+    except KeyboardInterrupt:
+        break
+
+    # if there's a problem reading a frame, break gracefully
+    except AttributeError:
+        break
+
+# stop the FPS counter timer
+fps.stop()
+
+# if "ctrl+c" is pressed in the terminal, break from the loop
+# destroy all windows if we are displaying them
+if args["display"] > 0:
+    cv2.destroyAllWindows()
+
+# stop the video stream
+#vs.stop()
+
+# clean up the graph and device
+graph.DeallocateGraph()
+device.CloseDevice()
+
+# display FPS information
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Railclippingcomputervision
+Railclippingcomputervision
 Raspberry Pi + intel movidius powered automatic clipping machine code for controlling Cembre Fastclip Clipping
 
 this project was done in 2018 December and testing completed in April 2019. The aim of the project was to:
 
 train a CNN custom object detection model (Mobilenet) with library of circa 1000 real site Fasclip photos.
-Fully automate Cembre Fast Clip type clipping machine using a CSI Camera input into Raspberry pi with Intel Movidius NCS first gen. chip acting as GPU.
+Fully automate Cembre Fast Clip type clipping machine using a CSI Camera input into Raspberry pi with Intel Movidius first gen. chip acting as GPU.
 There were 2 x actuators installed on the retrofitted Clipping machine
 
 No.01 24v DC actuator was to give up and down motion between the clips as done by human operator
@@ -20,4 +20,4 @@ Timer on step no. 03 was fine tuned to reach travel distance.
 Activate No.02 actuator to increase hydraulic pressure to perform Clipping or Unclipping operation as required.
 once operation complete in step 5, step 4 and step 3 were carried out in reverse to lift the clipping machine back to travel position
 Once step 6 complete (limit switch) engage the H bridge to engage DC motor forward motion.
-please refer attached photos and videos and feel free to message me here for any queries swamydce@gmail.com
+please refer attached photos and videos and feel free to message me here for any queries swamy.dce@gmail.com
